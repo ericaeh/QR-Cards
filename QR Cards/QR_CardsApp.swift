afa1ebe0a945d21185b10875a0b599c63df07104
ericaeh <erica.heiermann@icloud.com>
2025-10-31 22:47:46 +0000

    Initial Commit

---
+++ QR Cards/QR_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_CardsApp.swift
+//  QR Cards
+//
+//  Created by Erica Heiermann on 31/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct QR_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
